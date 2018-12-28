9f71fceed5188063e6f28a25f0f4ad3d86213908
Simo Mazil <mohamed.mazil@gmail.com>
2018-12-29 00:21:31 +0100

    login with server side

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,9 @@
 import { USER_LOGGED_IN } from '../types';
 import api from '../api';
-import { dispatch } from 'rxjs/internal/observable/range';
 
 export const userLoggedIn = user => ({
   type: USER_LOGGED_IN,
   user
 })
 
-export const login = credentials => () => api.user.login(credentials).then(user => dispatch(userLoggedIn(user)))
+export const login = credentials => dispatch => api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
